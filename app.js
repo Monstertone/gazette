235a5f3eb6fccdf9145c8d5cb718d024cd4e2f6a
Thomas Gabauer <tgabauer@gmail.com>
2018-08-27 16:15:09 -0700

    first javascript working.  API call working

---
+++ app.js
@@ -0,0 +1,22 @@
+window.onload = function(e) {
+
+// THIS AREA IS TO GENERATE AND INSERT CURRENT DATE
+
+let Currentdate = new Date();
+let year = Currentdate.getFullYear();
+let month = Currentdate.getMonth();
+let day = Currentdate.getDay();
+let date =Currentdate.getDate();
+let currentMonth = ["JANUARY","FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+let currentDay = ["SUNDAY","MONDAY","TUESDAY", "WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]
+let newsDate = currentDay[day] + " " + currentMonth[month]+ " " + date + ", " + year;
+
+let insertDate = document.querySelector('.datebar');
+insertDate.innerHTML = newsDate;
+
+ axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=6560880c487746438fe80efea6edbe2d').then(function(response){
+
+   console.log(response.data.articles[0]);
+ });
+
+}
