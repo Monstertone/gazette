a4b3d25342ad592c2b61e07a10d1c86d77fdfe13
Thomas Gabauer <tgabauer@gmail.com>
2018-08-29 09:12:11 -0700

    got javascript custom search functionality working

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var containerPush = document.getElementById('newsContainer')
+
 window.onload = function(e) {
 
 // THIS AREA IS TO GENERATE AND INSERT CURRENT DATE
@@ -14,9 +16,16 @@ let newsDate = currentDay[day] + " " + currentMonth[month]+ " " + date + ", " + 
 let insertDate = document.querySelector('.datebar');
 insertDate.innerHTML = newsDate;
 
-// THIS AREA PRODUCES THE INITIAL GRAB OF 20 LATEST STORIES AND PLACES THEM ON THE FRONT PAGE
+//SEARCH RETURNS 20 LATEST TOP-HEADLINE STORIES TO APPEAR ON LOAD
 
- axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=6560880c487746438fe80efea6edbe2d').then(function(response){
+let latestNews = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=6560880c487746438fe80efea6edbe2d'
+getNews(latestNews)
+
+};
+
+// FUNCION TO PASS SEARCH STRING TO API AND PRINT RESULTS TO MAIN PAGE
+
+ function getNews(apiSearch){axios.get(apiSearch).then(function(response){
     let storiesArray = response.data.articles;
     let storyCount = storiesArray.length;
 
@@ -29,7 +38,7 @@ insertDate.innerHTML = newsDate;
        let getDescription = response.data.articles[i]['description'];
        let getAuthor = response.data.articles[i]['author'];
            if (!getAuthor){
-             getAuthor= "Author not provided    ";
+             getAuthor= "Author not provided";
            }
        let getSource = response.data.articles[i]['source']['name'];
            if (!getSource) {
@@ -52,9 +61,9 @@ insertDate.innerHTML = newsDate;
            newUrl.setAttribute("href", getUrl)
            newImg.setAttribute("src", getImg);
 
-           newAuthor.innerHTML = (`Author:&#160&#160${getAuthor}&#160&#160&#160&#160&#160 Source:${getSource}`)
+           newAuthor.innerHTML = (`Author:&#160&#160${getAuthor}&#160&#160&#160&#160&#160 Source:&#160&#160${getSource}`)
 
-       let containerPush = document.getElementById('newsContainer')
+       // let containerPush = document.getElementById('newsContainer')
        let storyPush = document.querySelector('.storyDiv');
        let newPush = storyPush.cloneNode(true);
 
@@ -69,6 +78,13 @@ insertDate.innerHTML = newsDate;
 
             searchNews.addEventListener('click', function(e){
               e.preventDefault();
-              console.log("click confirmed")
+            let searchVal = searchTopic.value;
+            let customSearch = `https://newsapi.org/v2/everything?q=${searchVal}&apiKey=6560880c487746438fe80efea6edbe2d`
+
+             if (searchVal) {
+               containerPush.innerHTML = "";
+               getNews(customSearch);
+             }
+
       })
 }
